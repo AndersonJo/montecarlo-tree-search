84d950d2f3486ca89d3c81c00e71643498f61ff4
Anderson <a141890@gmail.com>
2019-02-08 16:02:01 +0900

    Dev tic tac toe

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,9 +1,7 @@
-from abc import ABC
-
+import numpy as np
 import pygame
 from gym.utils import EzPickle
 from pygame.locals import *
-import numpy as np
 
 from mcts.env import BaseEnv
 
@@ -12,12 +10,12 @@ class TicTacToeBase(BaseEnv, EzPickle):
     EMPTY = 0
     WHITE = 1
     BLACK = 2
-    PLAYERS = {1: 'white', 2: 'black'}
+    PLAYERS = {1: 'O', 2: 'X'}
 
     def __init__(self):
         super(TicTacToeBase, self).__init__()
-        self.player = 0
-        self.board = np.zeros(3, 3)
+        self.player = self.WHITE
+        self.board = np.zeros((3, 3))
 
     def calculate_reward(self, player) -> int:
         pass
@@ -32,7 +30,7 @@ class TicTacToeBase(BaseEnv, EzPickle):
         pass
 
     def get_legal_actions(self) -> list:
-        pass
+        return np.column_stack(np.dstack(np.where(self.board == self.EMPTY))).tolist()
 
     def next_player(self):
         pass
@@ -41,21 +39,38 @@ class TicTacToeBase(BaseEnv, EzPickle):
         pass
 
     def to_hashed_state(self, player, state) -> str:
-        pass
+        return str(self.player) + ''.join(self.board.reshape(-1).astype(np.int).astype(np.str).tolist())
 
     def step(self, action):
+
         y, x = action
         assert self.board[y, x] == self.EMPTY
 
         self.board[y, x] = self.player
 
-        self.change_turn()
+        self.is_win()
 
     def reset(self):
         pass
 
     def render(self, mode='human'):
         pass
+
+    def is_win(self) -> bool:
+        for i in range(0, 3):
+            # Checks rows and columns for match
+            rows_win = (self.board[i, :] == self.player).all()
+            cols_win = (self.board[:, i] == self.player).all()
+
+            if rows_win or cols_win:
+                return True
+
+        diag1_win = (np.diag(self.board) == self.player).all()
+        diag2_win = (np.diag(np.fliplr(self.board)) == self.player).all()
+
+        if diag1_win or diag2_win:
+            return True
+        return False
 
 
 class TicTacToe(TicTacToeBase):
@@ -65,24 +80,24 @@ class TicTacToe(TicTacToeBase):
 
         # screen: initialized pygame screen
         # background: game board surface
+        self.init_screen: pygame.Surface = None
         self.screen: pygame.Surface = None
-        self.background: pygame.Surface = None
         self.init_board()
 
     def init_board(self):
         pygame.init()
-        self.screen: pygame.Surface = pygame.display.set_mode((300, 325))
+        self.init_screen: pygame.Surface = pygame.display.set_mode((300, 325))
         pygame.display.set_caption('Tic-Tac-Toe')
 
-        self.background = pygame.Surface(self.screen.get_size())
-        self.background = self.background.convert()
-        self.background.fill((250, 250, 250))
+        self.screen = pygame.Surface(self.init_screen.get_size())
+        self.screen = self.screen.convert()
+        self.screen.fill((250, 250, 250))
 
-        pygame.draw.line(self.background, (0, 0, 0), (100, 0), (100, 300), 2)
-        pygame.draw.line(self.background, (0, 0, 0), (200, 0), (200, 300), 2)
+        pygame.draw.line(self.screen, (0, 0, 0), (100, 0), (100, 300), 2)
+        pygame.draw.line(self.screen, (0, 0, 0), (200, 0), (200, 300), 2)
 
-        pygame.draw.line(self.background, (0, 0, 0), (0, 100), (300, 100), 2)
-        pygame.draw.line(self.background, (0, 0, 0), (0, 200), (300, 200), 2)
+        pygame.draw.line(self.screen, (0, 0, 0), (0, 100), (300, 100), 2)
+        pygame.draw.line(self.screen, (0, 0, 0), (0, 200), (300, 200), 2)
 
     def play_as_human(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -92,7 +107,9 @@ class TicTacToe(TicTacToeBase):
             return None
 
         # draw an X or O
-        # drawMove(board, row, col, XO)
+        # self.move(y, x, self.player)
+        state, reward, done, info = self.step((y, x))
+        print(f'state:{state} | reward:{reward} | done:{done}')
 
     def to_board_position(self, mouse_y, mouse_x):
         if mouse_y < 100:
@@ -116,7 +133,7 @@ class TicTacToe(TicTacToeBase):
     def _render_status(self):
         winner = None
         if (winner is None):
-            message = self.PLAYERS[self.player] + "'s turn"
+            message = f'Turn: {self.PLAYERS[self.player]}'
         else:
             message = winner + " won!"
 
@@ -130,7 +147,7 @@ class TicTacToe(TicTacToeBase):
 
     def render(self, mode='human'):
         self._render_status()
-        self.screen.blit(self.background, (0, 0))
+        self.init_screen.blit(self.screen, (0, 0))
         pygame.display.flip()
 
     def play(self):
@@ -150,63 +167,63 @@ class TicTacToe(TicTacToeBase):
                 # check for a winner
                 self.render()
 
-                # update the display
-                showBoard(ttt, board)
+    def step(self, action, mode='human'):
+        super(TicTacToe, self).step(action)
+        y, x = action
 
-    def move(self, y, x, player):
         # determine the center of the square
         center_x = (x * 100) + 50
         center_y = (y * 100) + 50
 
         # draw the appropriate piece
-        if player == self.WHITE:
-            pygame.draw.circle(self.background, (0, 0, 0), (center_x, center_y), 44, 2)
+        if self.player == self.WHITE:
+            pygame.draw.circle(self.screen, (0, 0, 0), (center_x, center_y), 44, 2)
         else:
-            pygame.draw.line(self.background, (0, 0, 0), (center_x - 22, center_y - 22), \
+            pygame.draw.line(self.screen, (0, 0, 0), (center_x - 22, center_y - 22), \
                              (center_x + 22, center_y + 22), 2)
-            pygame.draw.line(self.background, (0, 0, 0), (center_x + 22, center_y - 22), \
+            pygame.draw.line(self.screen, (0, 0, 0), (center_x + 22, center_y - 22), \
                              (center_x - 22, center_y + 22), 2)
 
-        # mark the space as used
-        self.board[y, x] = player
+        done = is_win = self.is_win()
+        hashed_state = self.to_hashed_state(self.player, self.board)
+        reward = 0
+
+        if not done:
+            done = np.sum(self.board == self.EMPTY) == 0
+        if is_win:
+            reward = 1
+
+        pygame.display.update()
+
+        self.change_turn()
+        return hashed_state, reward, done, {}
+
+    def is_win(self) -> bool:
+        for i in range(0, 3):
+            # Checks rows and columns for match
+            rows_win = (self.board[i, :] == self.player).all()
+            cols_win = (self.board[:, i] == self.player).all()
+
+            if rows_win:
+                pygame.draw.line(self.screen, (250, 0, 0), (0, (i + 1) * 100 - 50), (300, (i + 1) * 100 - 50), 2)
+                return True
+            elif cols_win:
+                pygame.draw.line(self.screen, (250, 0, 0), ((i + 1) * 100 - 50, 0), ((i + 1) * 100 - 50, 300), 2)
+                return True
+
+        diag1_win = (np.diag(self.board) == self.player).all()
+        diag2_win = (np.diag(np.fliplr(self.board)) == self.player).all()
+
+        if diag1_win:
+            pygame.draw.line(self.screen, (250, 0, 0), (50, 50), (250, 250), 2)
+            return True
+        elif diag2_win:
+            pygame.draw.line(self.screen, (250, 0, 0), (250, 50), (50, 250), 2)
+            return True
+
+        return False
 
 
-def gameWon(board):
-    # determine if anyone has won the game
-    # ---------------------------------------------------------------
-    # board : the game board surface
-
-    global grid, winner
-
-    # check for winning rows
-    for row in range(0, 3):
-        if ((grid[row][0] == grid[row][1] == grid[row][2]) and \
-                (grid[row][0] is not None)):
-            # this row won
-            winner = grid[row][0]
-            pygame.draw.line(board, (250, 0, 0), (0, (row + 1) * 100 - 50), \
-                             (300, (row + 1) * 100 - 50), 2)
-            break
-
-    # check for winning columns
-    for col in range(0, 3):
-        if (grid[0][col] == grid[1][col] == grid[2][col]) and \
-                (grid[0][col] is not None):
-            # this column won
-            winner = grid[0][col]
-            pygame.draw.line(board, (250, 0, 0), ((col + 1) * 100 - 50, 0), \
-                             ((col + 1) * 100 - 50, 300), 2)
-            break
-
-    # check for diagonal winners
-    if (grid[0][0] == grid[1][1] == grid[2][2]) and \
-            (grid[0][0] is not None):
-        # game won diagonally left to right
-        winner = grid[0][0]
-        pygame.draw.line(board, (250, 0, 0), (50, 50), (250, 250), 2)
-
-    if (grid[0][2] == grid[1][1] == grid[2][0]) and \
-            (grid[0][2] is not None):
-        # game won diagonally right to left
-        winner = grid[0][2]
-        pygame.draw.line(board, (250, 0, 0), (250, 50), (50, 250), 2)
+if __name__ == '__main__':
+    ttt = TicTacToe()
+    ttt.play()
